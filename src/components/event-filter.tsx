e09d8a9db1ac945b7cc7811b9917cbf999eb81f0
CarterWu <nkwuyanbiao@163.com>
2026-02-12 10:11:44 +0800

    Remove DingTalk initialization script and friend links
    Cleaned up inline DingTalk event handling script from layout and removed friend link elements from event filter component.

--- src/components/event-filter.tsx
+++ src/components/event-filter.tsx
@@ -273,8 +273,6 @@ export function EventFilter({ onEventTypeChange, onOrganizerChange, onTagsChange
           </div>
         </div>
       </CardContent>
-      <a href="#" className='friend-links cursor-pointer text-blue-600 hover:text-blue-800 block text-center py-2' onClick={(e) => e.preventDefault()}> 友情连接</a>
-            <a href="#" className='friend-links cursor-pointer text-blue-600 hover:text-blue-800 block text-center py-2' onClick={(e) => e.preventDefault()}> 友情连接2</a>
     </Card>
   )
 }
